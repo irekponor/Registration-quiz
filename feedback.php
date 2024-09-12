8c055bb585e61af0c270f3d7314451a4111fd0db
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-12 23:54:59 +0100

    feedback

---
+++ feedback.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="feedback.css" type="text/css">
+</head>
+
+<body>
+    <div class="container">
+        <form action="process.php" method="post">
+            <input type="email" name="email" id="" placeholder="Email">
+            <textarea name="message" id="" cols="30" rows="10" placeholder="Message"></textarea>
+            <input type="submit" class="btn btn-primary mt-4" value="Send" name="submit">
+        </form>
+    </div>
+</body>
+
+</html>
