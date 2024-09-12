1f616ec771779b8071947707bfe9b12eaa935192
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-12 11:33:26 +0100

    added quiz.php

--- home.php
+++ home.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["user"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quiz</title>
+    <title>quiz</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="home.css" type="text/css">
     <script src="https://kit.fontawesome.com/de8e2530fe.js" crossorigin="anonymous"></script>
@@ -24,7 +24,7 @@ if (!isset($_SESSION["user"])) {
         <p>Welcome to Emmanuel's comprehensive 20-question quiz, designed to challenge your expertise across a
             wide range of subjects!. This quiz will take you on a journey through various fields,
             including history, science, literature, music, and more. Get ready to put your knowledge
-            to the test and discover new fascinating facts. Good luck, and have fun!</p>
+            to the test and discover new fascinating facts. Good luck, and have fun!.</p>
 
         <a href="quiz.php" class="btn btn-dark">Start</a>
 
