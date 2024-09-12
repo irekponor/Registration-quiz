eae5e37c8d778f9bc1959a0f95543e5bc5a03f17
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-12 12:48:14 +0100

    added quiz.php

--- save-score.php
+++ save-score.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 require_once "database.php";
 
 $score = $_POST["score"];
@@ -15,3 +16,30 @@ $stmt->bindParam(3, $failed_questions);
 $stmt->execute();
 
 echo "Score saved successfully!";
+
+try {
+    $query = "INSERT INTO quiz_scores (email, score, failed_questions) VALUES (:email, :score, :failed_questions, NOW());";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":score", $score);
+    $stmt->bindParam(":failed_questions", $failed_questions);
+
+
+    $stmt->execute();
+
+    $pdo = null;
+    $stmt = null;
+    
+        try {
+            $query = "INSERT INTO tasks (tasks) VALUES (:tasks);";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(":tasks", $tasks);
+            $stmt->execute();
+            $pdo = null;
+            $stmt = null;
+            header("Location: index.php");
+            die();
+        } catch (PDOException $e) {
+            die("Query Failed:" . $e->getMessage());
+        }
+    }
