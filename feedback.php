03c80215e6c12e775e74b5a7fea22f5a5faf51a7
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-13 00:05:51 +0100

    feedback

--- feedback.php
+++ feedback.php
@@ -4,17 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Feedback</title>
     <link rel="stylesheet" href="feedback.css" type="text/css">
 </head>
 
 <body>
     <div class="container">
-        <form method="post">
-            <input type="email" name="email" id="" placeholder="Email">
-            <textarea name="message" id="" cols="30" rows="10" placeholder="Message"></textarea>
-            <input type="submit" class="btn btn-primary mt-4" value="Send" name="submit">
+        <form onsubmit="emailSend(); reset(); return false;">
+            <p>Full Name:</p>
+            <input type="text" name="senderName" id="senderName" placeholder="" required="required" />
+            <p>Email Add:</p>
+            <input type="email" name="senderEmail" id="Email_id" placeholder="" required="required" />
+            <p>Subject:</p>
+            <input type="text" name="subject" id="subject" placeholder="" required="required" />
+            <p>Message:</p>
+            <textarea name="message" id="message" rows="4" placeholder="" required="required"></textarea>
+            <button type="submit" id="sendMessage" class="c-btn pull-right">Submit</button>
         </form>
     </div>
 </body>
