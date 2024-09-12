8a5c1a401144ab433b78842ae39642ef90dbee0b
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-13 00:38:15 +0100

    feedback

---
+++ feedback.js
@@ -0,0 +1,40 @@
+function emailSend() {
+  var senderName = document.getElementById("senderName").value;
+  var Email_id = document.getElementById("Email_id").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+
+  var message =
+    "Sendername: " +
+    senderName +
+    "<br/> Email_id: " +
+    Email_id +
+    "<br/> Subject: " +
+    subject +
+    "<br/> Message: " +
+    message;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "emmanuelirekponor86@gmail.com",
+    Password: "845AB154985535014110CDF2BD9A90B71E40",
+    To: "emmanuelirekponor@outlook.com",
+    From: "emmanuelirekponor@outlook.com",
+    Subject: "from the quiz app",
+    Body: message,
+  }).then((message) => {
+    if (message == "OK") {
+      swal(
+        "Sent successfully!",
+        "Thanks for sending your message! I'll get back to you shortly.",
+        "success"
+      );
+    } else {
+      swal(
+        "Error!",
+        "There was a problem sending your message. Please try again.",
+        "error"
+      );
+    }
+  });
+}
