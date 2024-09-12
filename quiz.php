c8b69765267ac928a2610cf841d264e35c02e433
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-12 12:11:49 +0100

    added quiz.php

--- quiz.php
+++ quiz.php
@@ -13,7 +13,6 @@ if (!isset($_SESSION["user"])) {
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="quiz.css" type="text/css">
     <script src="https://kit.fontawesome.com/de8e2530fe.js" crossorigin="anonymous"></script>
 
@@ -51,7 +50,9 @@ if (!isset($_SESSION["user"])) {
                 </li>
             </ul>
 
-            <p><a href="logout.php" class="btn btn-warning">Log-Out</a></p>
+            <button>
+                <a href="logout.php" class="btn btn-warning">Log-Out</a>
+            </button>
 
         </div>
 
