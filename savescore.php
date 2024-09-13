31aa69b44d0f1381abaec2a0628d951e64c6652a
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-13 04:06:35 +0100

    score allocation

--- savescore.php
+++ savescore.php
@@ -4,14 +4,14 @@ session_start();
 require_once "database.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
+    $userEmail = $_POST["email"];
     $score = $_POST["score"];
 
     if (isset($email) && isset($score)) {
         try {
             $query = "INSERT INTO quiz_scores (email, score) VALUES (:email, :score)";
             $stmt = $pdo->prepare($query);
-            $stmt->bindParam(":email", $email);
+            $stmt->bindParam(":email", $userEmail);
             $stmt->bindParam(":score", $score);
             $stmt->execute();
 
