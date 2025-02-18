73403b7e5520c3691433160fa612043aab4a5a57
emmanuel <emmanuelirekponor86@gmail.com>
2025-02-18 13:32:03 +0100

    modified the questions

--- quiz.js
+++ quiz.js
@@ -83,7 +83,7 @@ const data = [
   },
   {
     question:
-      "11. Which university is nicknamed 'the university of first choice'?",
+      "11. Which university is known as 'the university of first choice' in Nigeria?",
     a: "UNILAG",
     b: "LASU",
     c: "OAU",
